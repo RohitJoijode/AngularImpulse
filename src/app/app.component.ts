299d23ea302b00af1981019d3b40cad8b34fb363
@RohitJoijode <rohitjoijode122@gmail.com>
2023-06-11 18:27:41 +0530

    Commited by Rohit Joijode (11-06-2023) 18:27

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,5 +8,6 @@ import { Component } from '@angular/core';
 export class AppComponent {
   //////Pull From Local Added on (11-06-2023 18:22)
   //////Pull From Remote Branch Added On (11-06-2023)
+  //////Confilct Created From Local Added On 11-06-2023 18:27
   title = 'Impulse';
 }
