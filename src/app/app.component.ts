e90813127eeaa57e32587b156e30ea85964d6ce0
@RohitJoijode <rohitjoijode122@gmail.com>
2023-06-11 18:16:34 +0530

    Commited by Rohit Joijode (11-06-2023)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,6 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  //////////////////Pull From Remote
+  //////Pull From Local
   title = 'Impulse';
 }
